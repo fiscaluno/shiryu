6b64f4368506b77569ba3eb2865c3752c442585c
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-04-11 16:03:01 -0300

    update assets path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'VuejsLogin/static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/VuejsLogin',
 
     /**
      * Source Maps
