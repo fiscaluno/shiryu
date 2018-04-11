c9a4df0ee9af1f5494f8b19182ec39194c8a8046
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-04-11 15:26:40 -0300

    update assets path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'VuejsLogin/static/',
+    assetsPublicPath: 'https://github.com/viniciuslcpereira97/VuejsLogin/tree/gh-pages/static',
 
     /**
      * Source Maps
