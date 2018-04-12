372a744cb5767ed2786b4bd10661286e94cc42c1
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-04-12 17:11:51 -0300

    fix assets public path

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'VuejsLogin/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
