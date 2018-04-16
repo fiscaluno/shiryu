590e951c67bf64997f1b4bd182e016595d7d30bf
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-04-16 15:40:45 -0300

    router view at dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,21 +1,32 @@
 <template lang="html">
-  <div class="row">
-    <div class="col l3">
-      <ul class="collection">
-        <li class="collection-item avatar">
-          <img src="http://materializecss.com/images/yuna.jpg" alt="" class="circle">
-          <span class="title">{{ user.name }}</span>
-          <p>{{ user.email }}<br>
-          </p>
-          <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-        </li>
-      </ul>
+  <div class="container-fluid">
+    <router-view></router-view>
+    <custom-header></custom-header>
+    <div class="row">
+      <div class="col l3">
+        <ul class="collection">
+          <li class="collection-item avatar">
+            <img src="http://materializecss.com/images/yuna.jpg" alt="" class="circle">
+            <span class="title">{{ user.name }}</span>
+            <p>{{ user.email }}<br>
+            </p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import CustomHeader from '@/components/CustomHeader';
+import CustomFooter from '@/components/CustomFooter';
+
 export default {
+  components: {
+    CustomHeader,
+    CustomFooter,
+  },
   props: [
     'user',
   ],
